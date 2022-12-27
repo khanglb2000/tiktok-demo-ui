6eb96500bc30558eea58b10cecf99cf2b55f5e2d
khanglb2000 <lambaokhang2000@gmail.com>
2022-12-27 15:47:42 +0700

    Search input logic

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img
+      <Image
         className={cx('avatar')}
         src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/6347565be1c472d11e05d40f34e815d6~c5_100x100.jpeg?x-expires=1671930000&x-signature=vp1p0IOxNcMrFLha0alqI6v1EG0%3D"
         alt="Kevin"
